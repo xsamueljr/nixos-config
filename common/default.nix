0e7c28b4136a3beb2ad34ac1a76ac2da1d67164e
xsamueljr <git@xsamueljr.anonaddy.me>
2026-04-07 18:16:56 +0200

    enable steam

--- common/default.nix
+++ common/default.nix
@@ -126,6 +126,8 @@ in
       bun
       go
     ]);
+
+  programs.steam.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
