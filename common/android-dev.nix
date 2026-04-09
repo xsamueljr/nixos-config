cced8d907efd1fbc276972b2b3c739172ba77d89
xsamueljr <git@xsamueljr.anonaddy.me>
2026-04-09 12:28:33 +0200

    add android tools

---
+++ common/android-dev.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+  nixpkgs.config.android_sdk.accept_license = true;
+
+  programs.adb.enable = true;
+  users.users.samuel.extraGroups = [
+    "kvm"
+    "abdusers"
+  ];
+
+  environment.systemPackages = with pkgs; [
+    android-tools
+    androidenv.androidPkgs.androidsdk
+    android-studio
+  ];
+}
