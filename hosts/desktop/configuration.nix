7af52434b99dc7b8e7255cb0672ef1bc33e10879
xsamueljr <git@xsamueljr.anonaddy.me>
2026-04-13 11:55:34 +0200

    preparing desktop config

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -5,11 +5,15 @@
 
 {
   imports = [
+    ./hardware-configuration.nix
     ../../common
     ../../common/nvidia-base.nix
   ];
 
   networking.hostName = "nixos-desktop";
+  
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   environment.systemPackages = with pkgs; [
     gamemode
